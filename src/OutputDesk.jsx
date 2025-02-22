3cbb27032919d5fd5bef6bdad0ca24b04e2aa85e
04rookie <atharva.ek@gmail.com>
2025-02-22 17:14:12 -0500

    first commit

---
+++ src/OutputDesk.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+function OutputDesk() {
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        djsplay: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        // overflowY: "auto",
+      }}
+    ></div>
+  );
+}
+
+export default OutputDesk;
---
+++ src/UploadDesk.jsx
@@ -0,0 +1,62 @@
+import { Button, Typography } from "@mui/material";
+import React from "react";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { styled } from '@mui/material/styles';
+
+function UploadDesk() {
+  const VisuallyHiddenInput = styled("input")({
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
+    height: 1,
+    overflow: "hidden",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    whiteSpace: "nowrap",
+    width: 1,
+  });
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        // overflowY: "auto",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "10%",
+        }}
+      >
+        <Typography variant="h4" alt="Upload your notes here">
+          Upload your notes here!
+        </Typography>
+        <Button
+          style={{ marginTop: "10%" }}
+          component="label"
+          role={undefined}
+          variant="contained"
+          tabIndex={-1}
+          startIcon={<CloudUploadIcon />}
+        >
+          Upload files
+          <VisuallyHiddenInput
+            type="file"
+            onChange={(event) => console.log(event.target.files)}
+            multiple
+          />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default UploadDesk;
