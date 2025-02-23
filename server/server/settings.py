0cf2a7ba0f58cd7e083eae6928c8cad0552a37e7
kcbhola3030 <kcbhola123@gmail.com>
2025-02-22 19:58:12 -0500

    cors fix

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,11 +37,38 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "quiz"
+    "quiz",
+    "corsheaders"
 
 ]
 
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Example: React frontend running on port 3000
+    "http://localhost:5173",
+    
+]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+]
+
+CORS_ALLOW_HEADERS = [
+    "content-type",
+    "authorization",
+    "x-csrftoken",
+]
+
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
